79c0f8ea5452a1372982db733650ebcae82d3a00
Yongye Tan <yongyetan1209@gmail.com>
2023-09-23 21:28:48 -0400

    Dynamically create our form inputs with our different ingredient choices and place the user inputs into state variables

--- Lab/barista-app/src/Components/baristaForm.jsx
+++ Lab/barista-app/src/Components/baristaForm.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { useState } from "react";
+import RecipeChoices from "./recipeChoices";
 
 const BaristaForm = () => {
+
+    const [inputs, setInputs] = useState({
+        'temperature': '',
+        'milk': '',
+        'syrup': '',
+        'blended': ''
+    });
+
+    const ingredients = {
+        'temperature': ['hot', 'lukewarm', 'cold'],
+        'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
+        'milk': ['cow', 'oat', 'goat', 'almond', 'none'],
+        'blended': ['yes', 'turbo', 'no']
+    }
 
     const onCheckAnswer = () => {
 
@@ -14,6 +30,61 @@ const BaristaForm = () => {
         <div>
             <h2>Hi, I'd like to order a:</h2>
             <form>
+                <h3>Temperature</h3>
+                <div className="answer-space" >
+                    {inputs["temperature"]}
+                </div>
+                <RecipeChoices
+                    handleChange={(e) => setInputs((prevState) => ({
+                        ...prevState,
+                        [e.target.name]: e.target.value,
+                    }))}
+                    label="temperature"
+                    choices={ingredients["temperature"]}
+                    checked={inputs["temperature"]}
+                />
+
+                <h3>milk</h3>
+                <div className="answer-space" >
+                    {inputs["milk"]}
+                </div>
+                <RecipeChoices
+                    handleChange={(e) => setInputs((prevState) => ({
+                        ...prevState,
+                        [e.target.name]: e.target.value,
+                    }))}
+                    label="milk"
+                    choices={ingredients["milk"]}
+                    checked={inputs["milk"]}
+                />
+
+                <h3>syrup</h3>
+                <div className="answer-space" >
+                    {inputs["syrup"]}
+                </div>
+                <RecipeChoices
+                    handleChange={(e) => setInputs((prevState) => ({
+                        ...prevState,
+                        [e.target.name]: e.target.value,
+                    }))}
+                    label="syrup"
+                    choices={ingredients["syrup"]}
+                    checked={inputs["syrup"]}
+                />
+
+                <h3>blended</h3>
+                <div className="answer-space" >
+                    {inputs["blended"]}
+                </div>
+                <RecipeChoices
+                    handleChange={(e) => setInputs((prevState) => ({
+                        ...prevState,
+                        [e.target.name]: e.target.value,
+                    }))}
+                    label="blended"
+                    choices={ingredients["blended"]}
+                    checked={inputs["blended"]}
+                />
 
             </form>
 
---
+++ Lab/barista-app/src/Components/recipeChoices.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+const RecipeChoices = ({ handleChange, label, choices, checked }) => {
+    return (
+        <div className="radio-buttons">
+            {choices &&
+                choices.map((choice) => (
+                    <li key={choice}>
+                        <input
+                            id={choice}
+                            value={choice}
+                            name={label}
+                            type="radio"
+                            onChange={handleChange}
+                            checked={checked == choice}
+                        />
+                        {choice}
+                    </li>
+                ))}
+        </div>
+    );
+};
+
+
+export default RecipeChoices;
