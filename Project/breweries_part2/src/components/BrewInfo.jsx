f313b179247aadf7096f42128cd93a1f456a21e7
Yongye Tan <yongyetan1209@gmail.com>
2023-10-21 21:19:00 -0400

    Add brew info component

--- Project/breweries_part2/src/components/BrewInfo.jsx
+++ Project/breweries_part2/src/components/BrewInfo.jsx
@@ -1,16 +1,30 @@
 import React from "react";
+import "./BrewInfo.css";
 import { useLocation } from 'react-router-dom';
 
 const BrewInfo = () => {
 
     // get the state from the Link
     const location = useLocation();
+    const data = location.state.brewData;
 
     return (
-        <>
-            <h1>HELLO brew info</h1>
-            {JSON.stringify(location)}
-        </>
+        <div className="details">
+            <h1>More info</h1>
+            <h4>name: {data.name}</h4>
+            <h4>brewery_type: {data.brewery_type}</h4>
+            <h4>address_1: {data.address_1}</h4>
+            <h4>city: {data.city}</h4>
+            <h4>state_province: {data.state_province}</h4>
+            <h4>postal_code: {data.postal_code}</h4>
+            <h4>country: {data.country}</h4>
+            <h4>longitude: {data.longitude}</h4>
+            <h4>latitude: {data.latitude}</h4>
+            <h4>phone: {data.phone}</h4>
+            <h4>website_url:<a href={data.website_url} style={{"justifyContent": "left"}}>{data.website_url}</a></h4>
+            <h4>state: {data.state}</h4>
+            <h4>street: {data.street}</h4>
+        </div>
     );
 }
 
