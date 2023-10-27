a2561b4f12e3bc14ae3164620186b48fcad53e29
Yongye Tan <yongyetan1209@gmail.com>
2023-10-27 00:11:17 -0400

    Finish DetailView page

--- Project/crewmate/src/components/Gallery/Card.jsx
+++ Project/crewmate/src/components/Gallery/Card.jsx
@@ -6,7 +6,7 @@ import person from "../../assets/person.png"
 const Card = ({ crew }) => {
     return (
         <div className="Card" id={crew.color}>
-            <Link to={`/${crew.id}`} id="link">
+            <Link to={`/${crew.id}`} id="link" state={{ "crewData": crew }}>
                 <img src={person} alt="person" width={200} />
                 <h3>Name of Crewmate: <span>{crew.name}</span></h3>
                 <h3>Speed of Crewmate: <span>{crew.speed}</span></h3>
--- Project/crewmate/src/components/Gallery/DetailView.jsx
+++ Project/crewmate/src/components/Gallery/DetailView.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import over from "../../assets/over.png"
+import { useParams, useLocation } from "react-router-dom";
+import "./DetailView.css";
 
 const DetailView = () => {
+
+    const { id } = useParams();
+    const location = useLocation();
+    const data = location.state.crewData;
+    console.log(data);
+    console.log(id)
+
     return (
-        <h1>HELLO</h1>
+        <div className="edit">
+            <h1>Crewmate:</h1>
+            <h2>Stats: </h2>
+            <h2>Name: {data.name}</h2>
+            <h2>Color: {data.color}</h2>
+            <h2>Speed: {data.speed}</h2>
+
+            <h2>You may want to find a Crewmate with more speed, this one is kind of slow 😬</h2>
+
+            <div className="container">
+                <img src={over} alt="picture" width={200} />
+                <button className="button">Wanna edit this Crewmate?</button>
+            </div>
+        </div>
     )
 };
 
